4ab7cdd9c128a0f22bd200d5b7438b647223d200
harunjeylan <harunjeylanwako@gmai.com>
2024-09-07 20:25:00 +0300

    Converted

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const { PassThrough } = require('stream');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 const app = express();
 const port = 8800;
 
@@ -18,24 +20,48 @@ app.get('/download', async (req, res) => {
             responseType: 'stream'
         });
 
-        // Set appropriate headers
-        res.setHeader('Content-Type', response.headers['content-type']);
-        res.setHeader('Content-Disposition', response.headers['content-disposition'] || 'attachment');
+        // Get the filename from the Content-Disposition header or use a default name
+        const contentDisposition = response.headers['content-disposition'];
+        let filename = 'downloaded_file';
+        if (contentDisposition) {
+            const filenameMatch = contentDisposition.match(/filename="?(.+)"?/i);
+            if (filenameMatch) {
+                filename = filenameMatch[1];
+            }
+        }
 
-        // Pipe the response directly to the client
-        response.data.pipe(res);
+        // Create a temporary file path
+        const tempFilePath = path.join(os.tmpdir(), filename);
+
+        // Pipe the response to a file
+        const writer = fs.createWriteStream(tempFilePath);
+        response.data.pipe(writer);
+
+        // Wait for the download to finish
+        await new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
+
+        // Send the file
+        res.sendFile(tempFilePath, {}, (err) => {
+            if (err) {
+                console.error('Error sending file:', err);
+                res.status(500).send('Error sending file');
+            }
+            // Delete the temporary file after sending
+            fs.unlink(tempFilePath, (unlinkErr) => {
+                if (unlinkErr) console.error('Error deleting temporary file:', unlinkErr);
+            });
+        });
 
     } catch (error) {
         console.error('Error downloading file:', error);
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
             res.status(error.response.status).send(`Error: ${error.response.statusText}`);
         } else if (error.request) {
-            // The request was made but no response was received
             res.status(503).send('Error: No response received from the server');
         } else {
-            // Something happened in setting up the request that triggered an Error
             res.status(500).send('Error downloading file');
         }
     }
